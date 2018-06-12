f7d1f2a9e383ea04e2ec5d28105dc4b6144511c4
thflatk2 <thflatk2@naver.com>
2018-06-12 17:04:31 +0900

    landing login register

--- Job/urls.py
+++ Job/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    url(r'^$', views.JobListView.as_view(), name='list'),
+    url(r'^detail/$', views.JobDetailView.as_view(), name='detail'),
 
 ]
 
--- Job/views.py
+++ Job/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class JobListView(TemplateView):
+    template_name = 'job_list.html'
+
+class JobDetailView(TemplateView):
+    template_name = 'job_detail.html'
