3275d045192c0df5113a3453be2ad710c86ca1d2
Cypress <thflatk2@naver.com>
2018-06-22 00:45:09 +0900

    Job v0.4

--- Job/views.py
+++ Job/views.py
@@ -11,6 +11,7 @@ from django.views.generic.edit import CreateView, FormView
 from django.core.urlresolvers import reverse_lazy
 from Account.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
@@ -24,6 +25,49 @@ class JobListView(TemplateView):
         context['Jobs'] = Job_info.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
 
         return context
+
+    def get(self, request, *args, **kwargs):
+        sort = request.GET.get('sort', '')
+        page = request.GET.get('page')
+        Job = Job_info.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+        paginator = Paginator(Job, 2)  # Show 25 contacts per page
+
+        if sort == 'date_early':
+            Jobs = Job_info.objects.all().order_by('start_date')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'date_late':
+            Jobs = Job_info.objects.all().order_by('-start_date')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'period_long':
+            Jobs = Job_info.objects.all().order_by('contract_period')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'period_short':
+            Jobs = Job_info.objects.all().order_by('-contract_period')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'salary':
+            Jobs = Job_info.objects.all().order_by('salary')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'age':
+            Jobs = Job_info.objects.all().order_by('age_level')
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        else:
+            try:
+                Jobs = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                Jobs = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                Jobs = paginator.page(paginator.num_pages)
+
+            return render(request, 'job_list.html', {'Jobs': Jobs})
+
 
 
 class JobDetailView(TemplateView):
