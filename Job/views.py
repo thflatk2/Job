1557331aed0e60220742d1ceb35ae4530bd3bd63
Cypress <thflatk2@naver.com>
2018-07-09 01:20:12 +0900

    Job V0.7

--- Job/views.py
+++ Job/views.py
@@ -12,6 +12,7 @@ from django.core.urlresolvers import reverse_lazy
 from Account.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 
 
@@ -102,8 +103,36 @@ class JobListView(TemplateView):
 
             context = {'Jobs': Jobs}
             return render(request, 'job_list.html', context)
-        elif sort == 'age':
-            Job = Job_info.objects.all().order_by('age_level')
+        elif sort == 'kindergarten':
+            Job = Job_info.objects.filter(Q(age_level__icontains='kindergarten'))
+            paginator = Paginator(Job, 10)
+            try:
+                Jobs = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                Jobs = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                Jobs = paginator.page(paginator.num_pages)
+
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'elementary':
+            Job = Job_info.objects.filter(Q(age_level__icontains='elementary'))
+            paginator = Paginator(Job, 10)
+            try:
+                Jobs = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                Jobs = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                Jobs = paginator.page(paginator.num_pages)
+
+            context = {'Jobs': Jobs}
+            return render(request, 'job_list.html', context)
+        elif sort == 'middle':
+            Job = Job_info.objects.filter(Q(age_level__icontains='middle'))
             paginator = Paginator(Job, 10)
             try:
                 Jobs = paginator.page(page)
